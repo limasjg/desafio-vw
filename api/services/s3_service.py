eac303e236dbb8c78069146e95f52bd43a7ea40e
limasjg <joaoguilhermelima7@gmail.com>
2025-07-22 09:56:09 -0300

    Fixing images error on s3

--- api/services/s3_service.py
+++ api/services/s3_service.py
@@ -11,8 +11,14 @@ def upload_file_to_s3(file: UploadFile, object_name: str = None) -> str:
 
     s3_client = boto3.client("s3", region_name=settings.AWS_REGION)
     try:
-        s3_client.upload_fileobj(file.file, settings.S3_BUCKET_NAME, object_name)
-        # Gera a URL do arquivo após o upload
+        # ExtraArgs para tornar o objeto público
+        s3_client.upload_fileobj(
+            file.file, 
+            settings.S3_BUCKET_NAME, 
+            object_name,
+            ExtraArgs={'ACL': 'public-read'}
+        )
+        
         url = f"https://{settings.S3_BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{object_name}"
         return url
     except NoCredentialsError:
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,31 @@ resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
 }
 
+# Define que o dono do bucket pode gerenciar ACLs nos objetos
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Habilita o uso de ACLs
+resource "aws_s3_bucket_acl" "main" {
+  depends_on = [aws_s3_bucket_ownership_controls.main]
+
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+# Permite que os objetos se tornem públicos (necessário para o ACL 'public-read')
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
