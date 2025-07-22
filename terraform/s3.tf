f46280daa7262614b3529f9a3d742c022a8b0f85
limasjg <joaoguilhermelima7@gmail.com>
2025-07-22 10:45:11 -0300

    Fixing subnet router

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -28,18 +28,18 @@ resource "aws_s3_bucket_public_access_block" "main" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.s3"
-  vpc_endpoint_type = "Gateway"
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.s3"
+#   vpc_endpoint_type = "Gateway"
 
-  # Associa o endpoint às tabelas de rota.
-  route_table_ids   = [aws_route_table.private.id, aws_route_table.public.id]
+#   # Associa o endpoint às tabelas de rota.
+#   route_table_ids   = [aws_route_table.private.id, aws_route_table.public.id]
 
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "vpce-s3-${var.env}"
-    }
-  )
-}
+#   tags = merge(
+#     local.common_tags,
+#     {
+#       Name = "vpce-s3-${var.env}"
+#     }
+#   )
+# }
